51874eb70448824a76cb9639cfb969f2a56e582d
sandeep <sandeep30032006@gmail.com>
2024-01-15 21:11:32 +0530

    sample web

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
-function anime() {
-    let text = document.getElementsByClassName("anime");
+var content = document.getElementById("content");
+var oops = document.getElementById("oops");
+if(content.childNodes.length === 0){
+    console.log("no")
+    oops.style.visibility = "visible";
+}else {
+    console.log("yes");
+    oops.style.visibility = "hidden";
 }
-
 
 
 
@@ -10,7 +15,4 @@ function Drop() {
     var txt1 = document.getElementById("drop-menu");
     txt.style.visibility = (txt.style.visibility === "hidden")?"visible":"hidden";
     txt.style.top = (txt.style.top === "-1vh")?"0vh":"-1vh";
-
-    
-
 }
